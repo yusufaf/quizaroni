58c099391a93307b4006ce90551351967c39c647
yusufaf <yusufafzal12@gmail.com>
2022-01-08 22:58:42 -0800

    Handle desktop / window zoom, styling changes, initial work on ViewFlashSet

---
+++ src/utilities/handleDesktopZoom.js
@@ -0,0 +1,17 @@
+export const handleDesktopZoom = () => {
+    const {devicePixelRatio, innerWidth, innerHeight } = window;
+    const {availHeight, availWidth} = window.screen;
+
+    const defaultWidth = 1920;
+    const rootSize = 16;
+    const browserZoomModifier = availWidth / innerWidth;
+
+    const newRootSize = rootSize * (innerWidth / defaultWidth) * browserZoomModifier;
+    const adjustedRootSize = rootSize / devicePixelRatio;
+    
+    console.log("New Calculated Root Size = ", newRootSize);
+    console.log("rootSize / devicePixelRatio = ", adjustedRootSize);
+
+    console.log({innerHeight, innerWidth, devicePixelRatio, availHeight, availWidth});
+    document.querySelector("html").setAttribute("style", `font-size: ${adjustedRootSize}px`);
+}
