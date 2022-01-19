7b28408ff3a0b8df1e1a39fbf3a6b5dbd769b6b4
yusufaf <yusufafzal12@gmail.com>
2022-01-19 00:18:55 -0800

    Minor styling changes, install react-color dependency

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,3 +2,5 @@ export const SUCCESS = "success";
 export const SUCCESS_U = "Success";
 export const ERROR = "error";
 export const ERROR_U = "Error";
+
+export const DELETE_ACCOUNT_MSG = "We're sad to see you go, but if you're certain you want to delete your account, please confirm your password below.";
