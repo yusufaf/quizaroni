d84c7aa6925cb815dbdacc10fd01fa4153026142
yusufaf <yusufafzal12@gmail.com>
2022-01-30 12:32:21 -0800

    Add sending label to database, starting new component

---
+++ apps/web/src/Home/ViewFlashSet/ViewFlashCard.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react"
+
+/* Styling */
+import { useTheme } from "../../theme/useTheme";
+import * as viewFlashStyles from './ViewFlashSet.module.css';
+import * as appStyles from "../../App.module.css";
+
+export const ViewFlashCard = props => {
+
+}
