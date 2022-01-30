ad70d6ce75fb20865c272445c65628cd9679ec1d
yusufaf <yusufafzal12@gmail.com>
2022-01-30 00:11:51 -0800

    Add delete confirmation modal

--- src/Home/HomeFlashSet/HomeFlashSet.js
+++ src/Home/HomeFlashSet/HomeFlashSet.js
@@ -7,7 +7,17 @@ import { firebaseApp, database } from "../../firebase/firebase";
 
 /* Outside Components */
 import { Link, useNavigate } from "react-router-dom";
-import { Alert, AlertTitle, Tooltip } from '@mui/material/';
+
+import {
+    Alert,
+    AlertTitle,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Tooltip,
+} from '@mui/material/';
 import { ArrowBack } from '@mui/icons-material/';
 
 /* Styling */
@@ -28,31 +38,107 @@ const HomeFlashSet = props => {
     } = props.flashSet;
     const { isDarkMode, toggleDarkMode, theme } = useTheme();
 
+    // TODO: Possibly move this up to parent component, might not matter too much
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
+    const handleViewFlashset = e => {
+        let target = e.target;
+        if (target?.innerText !== "delete") {
+            setSelectedFlashSet(flashSet)
+            setViewFlashSet(true);
+        }
+    }
+
+    /* Flipping state */
+    const handleFavoriteSet = () => {
+        /* Update the set to be favorited in DB */
+        const flashCollection = collection(database, "flashcards");
+        const queryResult = query(flashCollection, where("uid", "==", uid));
+        console.log("queryResult in HomeFlashSet = ", queryResult);
+    }
+
+    const handleDeleteSet = () => {
+
+    }
+
+    useEffect(() => {
+        console.log("showDeleteConfirmation = ", showDeleteConfirmation);
+    }, [showDeleteConfirmation])
+
     return (
-        <div
-            className={`${homeFlashStyles.flashSet} ${isDarkMode ? `${appStyles.hoverDark} ${appStyles.darkInput}` : `${appStyles.hoverLight} ${appStyles.lightInput}`}`}
-            key={uid}
-            onClick={() => {
-                setSelectedFlashSet(flashSet)
-                setViewFlashSet(true);
-            }}
-        >
-            <div className={homeFlashStyles.data}>{title}</div>
-            <div className={homeFlashStyles.data}>{description}{description}{description}{description}</div>
-            <div className={homeFlashStyles.data}>{creationDate}</div>
-            <div className={homeFlashStyles.data}>{label || "N/A"}</div>
-            <Tooltip
-                title="Delete this card"
-                placement="bottom"
-                arrow={true}
+        <>
+            <div
+                className={`${homeFlashStyles.flashSet} ${isDarkMode ? `${appStyles.hoverDark} ${appStyles.darkInput}` : `${appStyles.hoverLight} ${appStyles.lightInput}`}`}
+                key={uid}
+                onClick={handleViewFlashset}
             >
-                <span className={appStyles.deleteCard}>
-                    <i className="material-icons-outlined" style={{ fontSize: "2rem" }}>
-                        delete
-                    </i>
-                </span>
-            </Tooltip>
-        </div>
+                <div className={homeFlashStyles.data}>{title}</div>
+                <div className={homeFlashStyles.data}>{description}</div>
+                <div className={homeFlashStyles.data}>{creationDate}</div>
+                <div className={homeFlashStyles.data}>{label || "N/A"}</div>
+                <div className={homeFlashStyles.actions}>
+                    <Tooltip
+                        title="Favorite this card"
+                        placement="right"
+                        arrow={true}
+                    >
+                        <span className={homeFlashStyles.favoriteCard}>
+                            <i className={`material-icons-outlined ${appStyles.clickIcon}`}>
+                                star_outline
+                            </i>
+                        </span>
+                    </Tooltip>
+                    <Tooltip
+                        title="Delete this card"
+                        placement="right"
+                        arrow={true}
+                    >
+                        <span
+                            className={homeFlashStyles.deleteCard}
+                            onClick={() => setShowDeleteConfirmation(true)}
+                        >
+                            <i className={`material-icons-outlined ${appStyles.clickIcon}`}>
+                                delete
+                            </i>
+                        </span>
+                    </Tooltip>
+                </div>
+            </div>
+
+            {showDeleteConfirmation &&
+                <Dialog
+                    open={showDeleteConfirmation}
+                    onClose={() => setShowDeleteConfirmation(false)}
+                    sx={{
+                        '& .MuiDialog-paper': {
+                            bottom: "10rem",
+                            color: theme.foreground,
+                            backgroundColor: theme.background
+                        },
+                    }}
+                >
+                    <DialogTitle>Delete this flashset?</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText
+                            sx={{
+                                color: theme.foreground,
+                                backgroundColor: theme.background
+                            }}
+                        >
+                            This will permanently delete the flashset.
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <div
+                            className={homeFlashStyles.confirmDeleteCard}
+                            onClick={handleDeleteSet}
+                        >
+                            Delete Card
+                        </div>
+                    </DialogActions>
+                </Dialog>
+            }
+        </>
     )
 }
 
