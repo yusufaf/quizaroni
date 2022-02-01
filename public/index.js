0d40e372e2933a66eedfdf2d968f35a9d6c6f9c6
yusufaf <yusufafzal12@gmail.com>
2022-01-31 23:21:57 -0800

    Switch to parcel bundler instead of webpack

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM, { render }  from 'react-dom';
+import { render }  from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import App from './App';
-import firebase from "./firebase/firebase"
+import App from '../src/App';
+import firebase from "../src/firebase/firebase"
 
 render(
   <BrowserRouter>
