1db73b11e83f02cb3fe201cf9bb48bd7088fae62
Yusuf Afzal <yusufafzal12@gmail.com>
2022-02-20 01:39:54 -0800

    Starting work on ViewFlashCard component, fix for Label Dropdown

--- src/Home/ViewFlashSet/ViewFlashCard.jsx
+++ src/Home/ViewFlashSet/ViewFlashCard.jsx
@@ -10,12 +10,28 @@ const ViewFlashCard = props => {
     const { isDarkMode, theme } = useTheme();
 
 
+    console.log("cardInfo in ", cardInfo);
+
     return (
         <div className={`${viewFlashStyles.viewFlashCard} ${index === 0 ? viewFlashStyles.firstCard : ""}`}
             key={index}
             style={{ color: theme.foreground, background: theme.background }}
         >
             <span><b>Card {index + 1}</b></span>
+            <div className={viewFlashStyles.viewCardContainer}>
+                <div className={viewFlashStyles.viewCardTerm}>
+                    <label className={appStyles.inputLabel}>Term</label>
+                    <span style={{ color: `${cardInfo?.textColor && cardInfo.textColor}` }}>
+                        {cardInfo.term}
+                    </span>
+                </div>
+                <div className={viewFlashStyles.viewCardDefinition}>
+                    <label className={appStyles.inputLabel}>Definition</label>
+                    <span style={{ color: `${cardInfo?.textColor && cardInfo.textColor}` }}>
+                        {cardInfo.definition}
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
