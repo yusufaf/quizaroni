c5049290d986d6954b8037b37c6b7cf3c3d232b2
Yusuf Afzal <yusufafzal12@gmail.com>
2022-03-17 16:21:35 -0700

    Starting Express REST api

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express()
+
+const port = process.env.PORT || 5000;
+
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// Testing a GET route
+app.get('/express_backend', (req, res) => {
+    res.send("<p>Hello</p>");
+
+    // res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+  });
