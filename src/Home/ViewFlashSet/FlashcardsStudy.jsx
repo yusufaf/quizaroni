ac7a60d19c5fd933197961c2f74b3351557b33be
Yusuf Afzal <yusufafzal12@gmail.com>
2022-03-28 23:38:39 -0700

    Initial progress on full screen study page

--- src/Home/ViewFlashSet/FlashcardsStudy.jsx
+++ src/Home/ViewFlashSet/FlashcardsStudy.jsx
@@ -1,29 +1,57 @@
 import { useState, useEffect, useRef } from "react"
 
 /* Firebase Operations */
-import { collection, addDoc, query, where, getDocs, updateDoc } from "firebase/firestore";
-import { database } from "../firebase/firebase";
+// import { collection, addDoc, query, where, getDocs, updateDoc } from "firebase/firestore";
+// import { database } from "../firebase/firebase";
 
 /* Outside Components */
-import { useNavigate } from "react-router-dom";
 import { Alert, AlertTitle, Modal, Tooltip } from '@mui/material/';
 
 /* Styling */
-import { useTheme } from "../theme/useTheme";
-import * as appStyles from "../App.module.css";
+import { useTheme } from "../../theme/useTheme";
+import * as appStyles from "../../App.module.css";
+import * as viewFlashStyles from './ViewFlashSet.module.css';
+
 // import {
 
 // } from "../utilities/constants";
 
 const FlashcardsStudy = props => {
-    const { userAuthState } = props;
+    const { userAuthState, selectedFlashSet, } = props;
     const { isDarkMode, theme } = useTheme();
 
-    
+    const [currentCard, setCurrentCard] = useState({});
+
+
+    const handleArrowClick = (direction) => {
+
+    }
 
     return (
-        <>
-        </>
+        <div className={viewFlashStyles.studyPage}>
+            {/* TODO: In global App stylings: define back arrow */}
+
+            {/* Progress Bar / Info here */}
+
+            <div className={viewFlashStyles.bruh}>
+                <div class={`material-icons ${viewFlashStyles.directionArrow}`}
+                    onClick={() => handleArrowClick("backward")}
+                >
+                    arrow_backward
+                </div>
+
+                <div>
+
+                </div>
+
+                <div class={`material-icons ${viewFlashStyles.directionArrow}`}
+                    onClick={() => handleArrowClick("forward")}
+                >
+                    arrow_forward
+
+                </div>
+            </div>
+        </div>
     );
 }
 
