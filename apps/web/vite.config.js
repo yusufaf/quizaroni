7eaaf3fa82ac6761b0698c528bbe7c8355185e29
Yusuf Afzal <yusufafzal12@gmail.com>
2022-04-10 14:21:02 -0700

    Testing creating SMTP server and working through authentication issues

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -5,17 +5,13 @@ import mix from 'vite-plugin-mix'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    mix({
-      handler: './api.js',
-    }
-    ),],
-  proxy: {
-    "/api": {
-      target: 'https://localhost:5000/',
-      changeOrigin: true,
-      secure: false,
-      ws: true,
-    }
-  }
+    react()],
+  // proxy: {
+  //   "/api": {
+  //     target: 'https://localhost:5000/',
+  //     changeOrigin: true,
+  //     secure: false,
+  //     ws: true,
+  //   }
+  // }
 })
