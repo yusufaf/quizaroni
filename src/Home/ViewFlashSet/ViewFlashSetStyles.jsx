6cb3c20c686cf024875f8bbd0fc1e3eb1f62f258
yusufaf <yusufafzal12@gmail.com>
2022-07-24 18:52:03 -0700

    More consolidation into styled components for view flashset page

---
+++ src/Home/ViewFlashSet/ViewFlashSetStyles.jsx
@@ -0,0 +1,36 @@
+import { styled } from "@mui/system";
+import { Box, Paper } from "@mui/material";
+
+export const ViewFlashsetPage = styled(Box)({
+    display: "grid",
+    justifyItems: "center",
+    alignItems: "center",
+})
+
+export const ViewFlashsetPaper = styled(Paper)({
+    marginTop: "2rem"
+})
+
+export const ViewFlashsetContainer = styled(Box)({
+    gridRow: "1",
+    height: "25rem",
+    width: "70rem",
+    padding: "1rem 1.25rem",
+    borderRadius: "0.75rem",
+})
+
+export const ViewFlashsetHeader = styled(Box)({
+    display: "grid",
+    gridTemplateColumns: "30rem auto",
+})
+
+export const SetInfo = styled(Box)(({ theme }) => ({
+    borderRight: `0.1rem solid ${theme.palette.text.primary}`,
+}));
+
+export const StudyModeGrid = styled(Box)({
+    marginTop: "1rem",
+    display: "grid",
+    gridTemplateColumns: "15rem 15rem",
+    gridTemplateRows: "auto auto",
+})
