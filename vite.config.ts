9071098027e1e88ed3d662adb4f87824ce97eef9
yusufaf <yusufafzal12@gmail.com>
2023-05-05 21:54:10 -0700

    Restyle categories dialog, update name of global slice, update vite config, label fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vitejs.dev/config/
 // TODO: https://vite-plugin-pwa.netlify.app/guide/#setup
 
 export default defineConfig({
+    define: {
+        global: {},
+    },
     plugins: [
         react(),
         // VitePWA({ registerType: "autoUpdate" }),
@@ -32,5 +34,6 @@ export default defineConfig({
                 ws: true,
             },
         },
+        port: 3000,
     },
 });
