f9615a9e5a1c63333b496a2f3c4d4c8b9684ebf7
yusufaf <yusufafzal12@gmail.com>
2023-07-28 22:00:29 -0700

    Add initial Label terminologies options in study set settings, move format terminologies into subcomponent.

--- apps/web/src/views/ViewStudySet/StudysetSettings/styles.tsx
+++ apps/web/src/views/ViewStudySet/StudysetSettings/styles.tsx
@@ -1,4 +1,4 @@
-import { Dialog, TextField } from '@mui/material';
+import { Dialog, DialogContent, TextField } from '@mui/material';
 import styled from "@emotion/styled";
 
 export const StyledDialog = styled(Dialog)({
@@ -11,6 +11,12 @@ export const StyledTextField = styled(TextField)({
     height: "2rem",
     width: "10rem",
 })
+
+export const StyledDialogContent = styled(DialogContent)({
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
+    gap: "2rem",
+});
 
 export const CustomInputsContainer = styled("div")({
     display: "flex", 
