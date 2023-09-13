5298f911a9c813cedb4fb188651b6a0c0da584d3
yusufaf <yusufafzal12@gmail.com>
2023-09-12 22:35:46 -0700

    Add right click context menu to home studysets table, update MUI versions, initial speech synthesis hook and chip width for view set cards

--- apps/web/src/views/ViewStudySet/ViewStudySetCard.tsx
+++ apps/web/src/views/ViewStudySet/ViewStudySetCard.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useTheme } from "theme/useTheme";
 import { Chip, IconButton, Tooltip, Typography } from "@mui/material/";
 import { SpeakerNotes, Star, StarBorder, VolumeUp } from "@mui/icons-material";
@@ -16,7 +16,9 @@ import { useMarkCardAsImportantMutation } from "state/api/studysets";
 import {
     DEFAULT_TERMINOLOGY,
     FORMAT_TERMINOLOGIES,
+    LABEL_TERMINOLOGIES,
 } from "utilities/constants";
+import useSpeechSynthesis from "lib/hooks/useSpeechSynthesis";
 
 type Props = {
     card: Card;
@@ -24,13 +26,27 @@ type Props = {
     selectedStudyset: Studyset;
 };
 
+// Helper function to calculate the total width of the chips
+const getTotalChipsWidth = (chips: any[]) => {
+    return chips.reduce((acc, chip) => {
+        const chipWidth = chip.offsetWidth;
+        return acc + chipWidth;
+    }, 0);
+};
+
 const ViewStudySetCard = (props: Props) => {
     const { card, index, selectedStudyset } = props;
 
     const { isDarkMode, theme } = useTheme();
-    const timeoutRef = useRef(null);
+    const { speak, cancel } = useSpeechSynthesis();
 
-    console.log({theme})
+    const [chipsToShowMap, setChipsToShowMap] = useState<{ [key: string]: number }>({});
+    const [isViewAllOpen, setIsViewAllOpen] = useState(false);
+
+    const timeoutRef = useRef(null);
+    const chipContainerRef = useRef(null);
+
+    const { categories = [] } = card;
 
     const selectedTerminology =
         selectedStudyset?.metadata?.terminology ?? DEFAULT_TERMINOLOGY;
@@ -38,8 +54,63 @@ const ViewStudySetCard = (props: Props) => {
         selectedTerminology === FORMAT_TERMINOLOGIES.CUSTOM
             ? selectedStudyset?.metadata?.customTerminology?.split("/") ?? []
             : selectedTerminology.split("/");
+    const selectedLabelTerminology =
+        selectedStudyset?.metadata?.labelTerminology ??
+        LABEL_TERMINOLOGIES.CARD;
+    const labelTerminology =
+        selectedLabelTerminology === LABEL_TERMINOLOGIES.CUSTOM
+            ? selectedStudyset?.metadata?.customLabelTerminology ?? ""
+            : selectedLabelTerminology;
 
     const [markCardAsImportant] = useMarkCardAsImportantMutation();
+
+    useEffect(() => {
+        const updateChipsDisplay = () => {
+            if (chipContainerRef.current) {
+                // @ts-ignore
+                const chips = chipContainerRef.current.querySelectorAll(".MuiChip-root") || [];
+                //@ts-ignore
+                const availableWidth = chipContainerRef.current.parentElement.offsetWidth;
+
+                let currentCard = "";
+                let visibleChips = 0;
+                let currentWidth = 0;
+                let chipsToShow: { [key: string]: number } = {};
+
+                chips.forEach((chip: Element) => {
+                    // @ts-ignore
+                    const chipWidth = chip.offsetWidth
+
+                    if (currentCard !== chip.id) {
+                        currentCard = chip.id;
+                        visibleChips = 0;
+                        currentWidth = 0;
+                    }
+
+                    if (currentWidth + chipWidth <= availableWidth) {
+                        visibleChips++;
+                        currentWidth += chipWidth;
+                    } else {
+                        chipsToShow[currentCard] = visibleChips;
+                        visibleChips = 1;
+                        currentWidth = chipWidth;
+                    }
+                });
+
+                // Update the last card
+                chipsToShow[currentCard] = visibleChips;
+
+                setChipsToShowMap(chipsToShow);
+            }
+        };
+
+        updateChipsDisplay();
+
+        window.addEventListener("resize", updateChipsDisplay);
+        return () => {
+            window.removeEventListener("resize", updateChipsDisplay);
+        };
+    }, [categories, chipContainerRef.current]);
 
     // TODO: Adjust to clear timeout / reset if clicked multiple times
     const handleAudioPlayback = () => {
@@ -64,6 +135,10 @@ const ViewStudySetCard = (props: Props) => {
         });
     };
 
+    const visibleChipsCount = chipsToShowMap[card.id] || 0;
+    const categoriesToDisplay = categories.slice(0, visibleChipsCount);
+    const remainingCategories = categories.slice(visibleChipsCount);
+
     return (
         <ViewFlashsetCard
             elevation={6}
@@ -79,10 +154,11 @@ const ViewStudySetCard = (props: Props) => {
             tabIndex={index}
         >
             <SimpleFlexContainer>
-                <BoldTypography variant="h5">Card {index + 1}</BoldTypography>
-                {/* TODO: Handle when there's too many categories, max width or a tooltip on a singular one? */}
-                <CategoryChips>
-                    {card?.categories?.map(
+                <BoldTypography variant="h5">
+                    {labelTerminology} {index + 1}
+                </BoldTypography>
+                <CategoryChips ref={chipContainerRef}>
+                    {categoriesToDisplay.map(
                         (category: string, index: number) => (
                             <CategoryChip
                                 key={index}
@@ -91,6 +167,14 @@ const ViewStudySetCard = (props: Props) => {
                                 title={category}
                             />
                         )
+                    )}
+                    {remainingCategories.length > 0 && (
+                        <CategoryChip
+                            label={`+${remainingCategories.length}`}
+                            variant="outlined"
+                            title={`View ${remainingCategories.length} more`}
+                            onClick={() => setIsViewAllOpen(true)}
+                        />
                     )}
                 </CategoryChips>
                 <ViewFlashCardActions>
@@ -102,17 +186,16 @@ const ViewStudySetCard = (props: Props) => {
                     <Tooltip title="Mark as important" placement="top">
                         <IconButton onClick={handleMarkAsImportant}>
                             {card.important ? (
-                                <Star sx={{ color: theme.palette.other.gold }} />
+                                <Star
+                                    sx={{ color: theme.palette.other.gold }}
+                                />
                             ) : (
                                 <StarBorder />
                             )}
                         </IconButton>
                     </Tooltip>
                     {/* TODO */}
-                    <Tooltip
-                        title="Open notes"
-                        placement="top"
-                    >
+                    <Tooltip title="Open notes" placement="top">
                         <IconButton>
                             <SpeakerNotes />
                         </IconButton>
