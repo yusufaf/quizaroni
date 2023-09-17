b659a258c64258f7027cb0b4b9b150a043440c8e
yusufaf <yusufafzal12@gmail.com>
2023-09-17 11:45:29 -0700

    Update EditableTextField, add initial note action buttons and hidden drawer view

--- src/components/EditableTextField/EditableTextField.tsx
+++ src/components/EditableTextField/EditableTextField.tsx
@@ -1,103 +1,72 @@
-import { useState } from "react";
 import {
-    IconButton,
-    InputAdornment,
     TextField,
-    Tooltip,
-} from '@mui/material/';
-import { Edit } from '@mui/icons-material/';
-import { styled } from "@mui/system";
+    Typography,
+} from "@mui/material/";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useTheme } from "theme/useTheme";
 
-const EditableTextField = props => {
-    const { style = {}, tooltipText = "", value } = props;
+type Props = {
+    isEditing: boolean;
+    style?: Object;
+    value: string;
+    setIsEditing: Dispatch<SetStateAction<boolean>>;
+}
+
+const EditableTextField = (props: Props) => {
+    const { isEditing, style = {}, value, setIsEditing } = props;
 
     const { isDarkMode, theme } = useTheme();
 
     const [editedValue, setEditedValue] = useState(value);
-    const [isEditing, setIsEditing] = useState(false)
-    const [mouseOver, setMouseOver] = useState(false)
+
+    const handleChange = (event) => {
+        setEditedValue(event.target.value);
+    };
 
     /* Logic for submitting changes to the backend
         TODO: Pass function down to component to execute in onBlur
     */
-    const handleOnBlur = () => {
+    const handleOnBlur = (event: any) => {
         setIsEditing(false);
     }
 
-    const handleChange = event => {
-        setEditedValue(e.target.value);
-    };
-
-    // const handleMouseEnter = event => {
-    //     if (!mouseOver) {
-    //         setMouseOver(true);
-    //     }
-    // };
-
-    // const handleMouseLeave = event => {
-    //     if (mouseOver) {
-    //         setMouseOver(false);
-    //     }
-    // };
-
-    const handleClick = () => {
-        setIsEditing(true);
-        setMouseOver(false);
-        inputRef.current.focus();
-    };
-
-    const StyledTextField = styled(TextField)({
-        borderBottom: 0,
-        "&:before": {
-            borderBottom: 0
-        },
-        ".Mui-disabled": {
-            borderBottom: 0,
-            opacity: 1,
-            WebkitTextFillColor: `${theme.palette.text.primary} !important`,
-            "&:before": {
-                borderBottom: 0
-            }
-        }
-    })
+    // const StyledTextField = styled(TextField)({
+    //     borderBottom: 0,
+    //     "&:before": {
+    //         borderBottom: 0,
+    //     },
+    //     ".Mui-disabled": {
+    //         borderBottom: 0,
+    //         opacity: 1,
+    //         WebkitTextFillColor: `${theme.palette.text.primary} !important`,
+    //         "&:before": {
+    //             borderBottom: 0,
+    //         },
+    //     },
+    // });
 
     return (
-        <div>
-            <StyledTextField
-                variant="standard"
-                defaultValue={value}
-                value={editedValue}
-                // error={editedValue === ""}
-                onBlur={handleOnBlur}
-                onChange={handleChange}
-                // readOnly={!isEditing}
-                disabled={!isEditing}
-                // onMouseEnter={handleMouseEnter}
-                // onMouseLeave={handleMouseLeave}
-                inputRef={input => input && input.focus()}
-                InputProps={{
-                    endAdornment: (
-                        <InputAdornment position="end">
-                            <Tooltip
-                                title={tooltipText}
-                                placement="right"
-                            >
-                                <IconButton onClick={handleClick}>
-                                    <Edit
-                                        sx={{
-                                            color: isEditing ? "orange" : ""
-                                        }} 
-                                    />
-                                </IconButton>
-                            </Tooltip>
-                        </InputAdornment>
-                    )
-                }}
-                sx={style}
-            />
-        </div>
-    )
-}
+        <>
+            {!isEditing ? (
+                <Typography sx={{wordBreak: "break-word", width: "100%"}}>
+                    {value}
+                </Typography>
+            ) : (
+                <TextField
+                    autoFocus
+                    variant="standard"
+                    fullWidth
+                    multiline
+                    defaultValue={value}
+                    value={editedValue}
+                    // error={editedValue === ""}
+                    onBlur={handleOnBlur}
+                    onChange={handleChange}
+                    sx={style}
+                />
+            )}
+        </>
+    );
+};
 
 export default EditableTextField;
