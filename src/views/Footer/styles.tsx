cb2bd15d568143f7ebfa6b17baca6b851ee91f5e
yusufaf <yusufafzal12@gmail.com>
2023-11-22 13:49:58 -0800

    Update sticky footer, move social links out of header and into footer

--- src/views/Footer/styles.tsx
+++ src/views/Footer/styles.tsx
@@ -1,10 +1,18 @@
+import { Paper } from "@mui/material";
 import { styled } from "@mui/system";
 
-export const StyledFooter = styled("footer")({
+export const StyledFooter = styled("footer")(({ theme }) => ({
     display: "flex",
     alignItems: "center",
     bottom: "0",
     height: "4rem",
     width: "100%",
     marginTop: "auto",
+    borderTop: `0.125rem solid ${theme.palette.divider}`,
+}));
+
+export const FooterPaper = styled(Paper)({
+    display: "flex",
+    width: "100%",
+    padding: "1rem 2rem 0 2rem",
 })
