e5b71c994ec4f3b362a37f1c1f58fa376af5fc02
yusufaf <yusufafzal12@gmail.com>
2024-02-24 16:58:08 -0800

    Create useFileUpload hook to simplify file upload and add AWS API calls

--- apps/web/src/api/awsAPI.ts
+++ apps/web/src/api/awsAPI.ts
@@ -1,47 +1,82 @@
-const BASE_API_URL = "https://30hl3jkdbg.execute-api.us-west-2.amazonaws.com/development/api";
-const AWS_API_KEY = import.meta.env.VITE_AWS_API_KEY
+import { Part, UUID } from "lib/types";
+
+const BASE_API_URL =
+    "https://30hl3jkdbg.execute-api.us-west-2.amazonaws.com/development/api";
+const AWS_API_KEY = import.meta.env.VITE_AWS_API_KEY;
 
 const COMMON_REQUEST_PROPS: RequestInit = {
     mode: "no-cors",
     credentials: "omit",
     headers: {
         "Content-Type": "application/json",
-        "X-Api-Key": AWS_API_KEY
+        "X-Api-Key": AWS_API_KEY,
     },
-}
+};
 
-export const initiateMultipartUpload = async () => {
-    const url = `${BASE_API_URL}/files/initiateMultipartUpload`
+type InitiateMultipartUploadProps = {
+    studysetUUID: UUID;
+    userUUID: UUID;
+    fileName: string;
+    contentType: string;
+};
+export const initiateMultipartUpload = async ({
+    studysetUUID = "",
+    userUUID,
+    fileName,
+    contentType,
+}: InitiateMultipartUploadProps) => {
+    const url = `${BASE_API_URL}/files/initiateMultipartUpload`;
     return await fetch(url, {
-        body: JSON.stringify({key: "deez"}),
+        body: JSON.stringify({ studysetUUID, userUUID, fileName, contentType }),
         method: "POST",
-        ...COMMON_REQUEST_PROPS
-    })
-}
+        ...COMMON_REQUEST_PROPS,
+    }).then((response) => response.json());
+};
 
-export const getMultipartSignedUploadUrls = async () => {
-    const url = `${BASE_API_URL}/files/getMultipartSignedUploadUrls`
+type GetMultipartSignedUploadUrlsProps = {
+    key: string;
+    uploadId: string;
+    numParts: number;
+};
+export const getMultipartSignedUploadUrls = async ({
+    key,
+    uploadId,
+    numParts,
+}: GetMultipartSignedUploadUrlsProps) => {
+    const url = `${BASE_API_URL}/files/getMultipartSignedUploadUrls`;
     return await fetch(url, {
-        body: JSON.stringify({key: "deez"}),
+        body: JSON.stringify({ key, uploadId, numParts }),
         method: "POST",
-        ...COMMON_REQUEST_PROPS
-    })
-}
+        ...COMMON_REQUEST_PROPS,
+    }).then((response) => response.json());
+};
 
-export const completeMultipartUpload = async () => {
-    const url = `${BASE_API_URL}/files/completeMultipartUpload`
+type CompleteMultipartUploadProps = {
+    key: string;
+    uploadId: string;
+    parts: Part[]
+};
+export const completeMultipartUpload = async ({
+    key,
+    uploadId,
+    parts,
+}: CompleteMultipartUploadProps) => {
+    const url = `${BASE_API_URL}/files/completeMultipartUpload`;
     return await fetch(url, {
-        body: JSON.stringify({key: "deez"}),
+        body: JSON.stringify({ key, uploadId, parts }),
         method: "POST",
-        ...COMMON_REQUEST_PROPS
-    })
-}
+        ...COMMON_REQUEST_PROPS,
+    }).then((response) => response.json());
+};
 
-export const deleteFile = async () => {
-    const url = `${BASE_API_URL}/files/deleteFile`
+type DeleteFileProps = {
+    key: string;
+};
+export const deleteFile = async ({ key }: DeleteFileProps) => {
+    const url = `${BASE_API_URL}/files/deleteFile`;
     return await fetch(url, {
-        body: JSON.stringify({key: "deez"}),
+        body: JSON.stringify({ key }),
         method: "POST",
-        ...COMMON_REQUEST_PROPS
-    })
-}
+        ...COMMON_REQUEST_PROPS,
+    }).then((response) => response.json());
+};
---
+++ apps/web/src/lib/hooks/useActionsStack.tsx
@@ -0,0 +1,20 @@
+import { useCallback, useEffect, useState } from "react";
+import { UUID } from "lib/types";
+
+type UseActionsStackProps = {
+    
+}
+
+const useActionsStack = (props: UseActionsStackProps) => {
+    const {
+
+    } = props;
+
+
+
+    return {
+
+    };
+};
+
+export default useActionsStack;
