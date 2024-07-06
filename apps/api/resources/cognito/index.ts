b5aa0f2452e3ea58395c36bc7d00937b8d83d36f
yusufaf <yusufafzal12@gmail.com>
2024-07-05 19:13:02 -0700

    Update to hardcoded user pool IDs

--- apps/api/resources/cognito/index.ts
+++ apps/api/resources/cognito/index.ts
@@ -1,9 +1,9 @@
 export const USER_POOL_IDS: { [key: string] : string } = {
-    development: "us-west-2_EWf08UnRF",
+    development: "us-west-2_lVho1De7z",
     production: "",
 }
 
 export const USER_POOL_CLIENT_IDS: { [key: string] : string } = {
-    development: "2veii63t0o6ebcmep0mgv1hpih",
+    development: "2s66bb95ckdolhcbvi7b4112av",
     production: "",
 }
